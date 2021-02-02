a8b50298fa505fc5cb36949e76ef444aee4aa91f
= <=>
2021-02-02 22:41:53 +0000

    updates

--- src/order/domain/model.py
+++ src/order/domain/model.py
@@ -11,11 +11,28 @@ class Asset:
     qty: int
     price: float
     currency: str = "GBP"
+    asset_type: str = "Equity"
 
+class Holdings:
+    def __init__(
+        self
+    ):
+        self._holdings = set()
+
+    def isAssetValid(self, asset_entry: Asset) -> bool:
+        return asset_entry.qty > 0 and asset_entry.price > 0
+    
+    def assetHoldings(self, asset_entry: Asset):
+        if self.isAssetValid(asset_entry):
+            return self._holdings.add(asset_entry)
+
+    @property
+    def test(self):
+        return self._holdings
 
 class Portfolio:
     def __init__(
-        self, id: str, holdings: set(), date_created: Optional[date], date_updated: Optional[date] ,
+        self, id: str, holdings: List[Asset], date_created: Optional[date], date_updated: Optional[date] ,
         total=  0.0):
         self.id = id
         self.holdings = holdings
@@ -27,8 +44,10 @@ class Portfolio:
         return self.id
 
     @property
-    def total(self) -> int:
+    def total(self) -> float:
         return sum(asset.qty * asset.price for asset in self.holdings)
+
+
 
 
 
--- tests/unit/test_order.py
+++ tests/unit/test_order.py
@@ -1,7 +1,11 @@
 import sys
 sys.path.insert(0, '/Users/temi/cosmic-code/code/src')
-from order.domain.model import Asset, Portfolio
+from order.domain.model import Asset, Portfolio, Holdings
 from datetime import date, time
+
+def create_asset():
+    new_asset = Asset(ticker = "abc", name="abc", qty = 10, price = 10.34)
+    return new_asset
 
 def test_asset_is_created():
     new_asset = Asset(ticker = "abc", name="abc", qty = 10, price = 10.34)
@@ -9,7 +13,7 @@ def test_asset_is_created():
     assert new_asset.ticker == "abc"
     assert new_asset.qty == 10
 
-def test_portfolio_is_created():
+def create_portfolio():
     all_holdings = list()
     new_asset = Asset(ticker = "abc", name="abc", qty = 10, price = 10.34)
     new_asset_2 = Asset(ticker = "abcb", name="abcb", qty = 50, price = 145.34)
@@ -17,7 +21,28 @@ def test_portfolio_is_created():
     all_holdings.append(new_asset)
     all_holdings.append(new_asset_2)
     all_holdings.append(new_asset_3)
-    total = sum(item.qty * item.price for item in all_holdings)
+    return all_holdings
+
+def test_portfolio_is_created():
+    all_holdings = create_portfolio()
     new_portfolio = Portfolio(id = 100, holdings=all_holdings, date_created=date, date_updated=date)
     assert new_portfolio.holdings == all_holdings
+
+def test_portfolio_total():
+    all_holdings = create_portfolio()
+    total = sum(item.qty * item.price for item in all_holdings)
+    new_portfolio = Portfolio(id = 100, holdings=all_holdings, date_created=date, date_updated=date)
     assert new_portfolio.total == total
+
+def test_asset_holding_is_valid():
+    test_asset = create_asset()
+    new_asset = Asset(ticker = "abc", name="abc", qty = 20, price = 10.34)
+    holding = Holdings()
+    assert holding.isAssetValid(new_asset) == True
+
+
+def test_asset_holding_allocation():
+    test_asset  = create_asset()
+    holding = Holdings().assetHoldings(test_asset)
+    assert holding[0].ticker == 'abc'
+ 
